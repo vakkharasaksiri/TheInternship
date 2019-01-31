35a81415bda47268815d90bfa56ab87ff20ddce1
Vashirawit Akkharasaksiri <vakkharasaksiri@hotmail.com>
2019-02-01 00:05:09 +0700

    Add files via upload

---
+++ weather/XML_JSON.py
@@ -0,0 +1,19 @@
+# Need additional library by <pip install xmltodict>
+import xmltodict
+import json
+import sys
+inputFile = sys.argv[1]
+
+with open(inputFile, 'r') as f:
+    xmlData = f.read()
+
+jsonData= json.dumps(xmltodict.parse(xmlData), indent=4)
+
+with open(inputFile[:-4]+".json", 'w') as f:
+    f.write(jsonData)
+
+#print("XML input (weather.xml):")
+#print(xmlString)
+
+#print("\nJSON output(weather.json):")
+#print(jsonString)
